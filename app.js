11c98d941af444c126ecdf4a64374cf05a8079a6
Jose Armando Felix Zaragoza <josearmando.zara@gmail.com>
2023-08-15 16:29:46 -0700

    HTML Implementation part 1

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 
-const fetch = require('node-fetch')
+
 const logger = require('morgan')
-const express = require('express')
 const errorHandler = require('errorhandler')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
 
+const fetch = require('node-fetch')
 const Prismic = require('@prismicio/client')
 const PrismicH = require('@prismicio/helpers')
 const uap = require('ua-parser-js')
@@ -20,20 +21,38 @@ const initApi = (req) => {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req,
     fetch,
-  });
-};
+  })
+}
+
+
+//Middleware to inject primic helper, ua-parser, and some utils
+app.use((req, res, next) => {
+  const ua = uap(req.headers['user-agent'])
+
+  res.locals.isDesktop = ua.device.type === undefined
+  res.locals.isPhone = ua.device.type === 'mobile'
+  res.locals.isTablet = ua.device.type === 'tablet'
+
+  /* res.locals.Numbers = index => {
+      return index == 0 ? 'One' : index == 1 ? 'Two' : index == 2 ? 'Three' : index == 3 ? 'Four' : ''
+  } */
+
+  res.locals.PrismicH = PrismicH
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 const handleRequest = async api => {
-  const [ meta, navigation, homepage, quoter-selectors, quoter-end ] =
+  const [ meta, navigation, homepage, quoter_selector, quoter_end ] =
     await Promise.all([
       api.getSingle('meta'),
-      api.getSingle('preloader'),
       api.getSingle('navigation'),
-      api.getSingle('home'),
-      api.getSingle('about'),
+      api.getSingle('homepage'),
+      api.getSingle('quoter_selector'),
+      api.getSingle('quoter_end'),
     ])
 
   /* const assets = []
@@ -46,8 +65,8 @@ const handleRequest = async api => {
     meta,
     navigation,
     homepage,
-    quoter-selectors,
-    quoter-end
+    quoter_selector,
+    quoter_end
   }
 }
 
@@ -55,9 +74,9 @@ app.get('/', async (req, res) => {
   const api = await initApi(req)
   const defaults = await handleRequest(api)
 
-  console.log(defaults.data)
+  console.log(defaults.homepage.data)
 
-  res.render('base', { defaults })
+  res.render('base', { ...defaults })
 })
 
 app.listen(port, () => {
