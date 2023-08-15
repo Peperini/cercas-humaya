57af5ed044e381e761d7c61f27812eb477c46ed5
Jose Armando Felix <josearmando.zara@gmail.com>
2023-08-14 22:36:12 -0700

    Prismic initial implementation with v7 (some fixes needed

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch')
+const logger = require('morgan')
 const express = require('express')
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client')
+const PrismicH = require('@prismicio/helpers')
+const uap = require('ua-parser-js')
+
+const initApi = (req) => {
+  return Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch,
+  });
+};
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('base')
+const handleRequest = async api => {
+  const [ meta, navigation, homepage, quoter-selectors, quoter-end ] =
+    await Promise.all([
+      api.getSingle('meta'),
+      api.getSingle('preloader'),
+      api.getSingle('navigation'),
+      api.getSingle('home'),
+      api.getSingle('about'),
+    ])
+
+  /* const assets = []
+
+  home.data.gallery.forEach(item => {
+      assets.push(item.image.url)
+  }) */
+
+  return {
+    meta,
+    navigation,
+    homepage,
+    quoter-selectors,
+    quoter-end
+  }
+}
+
+app.get('/', async (req, res) => {
+  const api = await initApi(req)
+  const defaults = await handleRequest(api)
+
+  console.log(defaults.data)
+
+  res.render('base', { defaults })
 })
 
 app.listen(port, () => {
